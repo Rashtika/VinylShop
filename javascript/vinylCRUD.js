45aed86260e5372d950a4a1e43e8d59e27a7a819
Rashtika <bstimac9009@gmail.com>
2024-06-23 11:25:08 +0200

    added crud methods for vinyls

--- javascript/vinylCRUD.js
+++ javascript/vinylCRUD.js
@@ -1,6 +1,14 @@
-const reviewsContainer = document.getElementById("vinyls-container");
+const vinylsContainer = document.getElementById("vinyls-container");
+const album = document.getElementById("album-name");
+const band = document.getElementById("band-name");
+const genre = document.getElementById("genre");
+const photoLink = document.getElementById("photo-link");
+const type = document.getElementById("type");
+const submitForm = document.getElementById("submitForm");
 
+//state
 let vinyls = [];
+let upd = "";
 
 function loadVinylsFromLocalStorage() {
     const vinylData = localStorage.getItem('vinyls');
@@ -28,6 +36,24 @@ function removeVynil(vinyl) {
     }
 }
 
+function removeVynilById(id) {
+    for (let i = 0; i < vinyls.length; i++) {
+        if (vinyls[i].id == id) {
+            vinyls.splice(i, 1);
+            break;
+        }
+    }
+}
+
+function getVynilById(id) {
+    for (let i = 0; i < vinyls.length; i++) {
+        if (vinyls[i].id == id) {
+            return vinyls[i];
+        }
+    }
+    return null;
+}
+
 function save() {
 
     let vinyl1 = {
@@ -35,7 +61,7 @@ function save() {
         albumName: "paris",
         bandName: "Filthy Frank",
         genre: "(T)RAP",
-        photoLink: "nema",
+        photoLink: "https://media.istockphoto.com/id/1278847693/photo/afro-american-tourist-under-the-eiffel-tower.jpg?s=612x612&w=0&k=20&c=_7RCsuc6yyNGEBfxEh63evjtTSmZvrJSWx7NXXeD2vs=",
         type: "EP"
     };
     vinyls.push(vinyl1);
@@ -45,7 +71,7 @@ function save() {
         albumName: "hamburger pls",
         bandName: "Filthy Frank",
         genre: "(T)RAP",
-        photoLink: "nema",
+        photoLink: "https://www.allrecipes.com/thmb/UsNtGp9OgIsKw6cPqGQ-CxLmnTE=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/AR-72657-best-hamburger-ever-ddmfs-4x3-hero-878e801ab30445988d007461782b3c25.jpg",
         type: "EP"
     };
     vinyls.push(vinyl2);
@@ -55,7 +81,7 @@ function save() {
         albumName: "YEAS",
         bandName: "Filthy Frank",
         genre: "(T)RAP",
-        photoLink: "nema",
+        photoLink: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTiJq43F3buQu8KqTjEDIjFcDTVa0fw81wq3Q&s",
         type: "EP"
     };
     vinyls.push(vinyl3);
@@ -71,20 +97,76 @@ document.addEventListener("DOMContentLoaded", () => {
         console.log("sad smo napravili");
     }
     else {
-
-        reviewsContainer.innerHTML = "";
-
-        vinyls.forEach((vinyl) => {
-            const reviewCard = document.createElement("div");
-            reviewCard.classList.add("review-card");
-
-            reviewCard.innerHTML += `
-                <br>${vinyl.albumName}<br>
-            `;
-
-            reviewsContainer.appendChild(reviewCard);
-        });
+        //clearVynilsFromLocalStorage();
 
     }
 
+    renderVinyls();
 })
+
+function renderVinyls() {
+    vinylsContainer.innerHTML = "";
+
+    vinyls.forEach((vinyl) => {
+        const vinylCard = document.createElement("div");
+        vinylCard.classList.add("review-card");
+
+        vinylCard.innerHTML += `
+                <br>${vinyl.albumName}<br>
+                <button id="${vinyl.id}-del" type="submit" onclick="clickDelButton(${vinyl.id});">DEL</button>
+                <button id="${vinyl.id}-upd" type="submit" onclick="clickUpdButton(${vinyl.id});">UPD</button>
+                <img src="${vinyl.photoLink}">
+            `;
+
+        vinylsContainer.appendChild(vinylCard);
+    });
+}
+
+function clickDelButton(id) {
+    removeVynilById(id);
+    saveVinylToLocalStorage(vinyls);
+    renderVinyls();
+}
+
+function clickUpdButton(id) {
+    let vinyl = getVynilById(id);
+
+    if (vinyl != null) {
+        album.value = vinyl.albumName;
+        band.value = vinyl.bandName;
+        genre.value = vinyl.genre;
+        photoLink.value = vinyl.photoLink;
+        type.value = vinyl.type;
+        upd = vinyl.id;
+    }
+}
+
+submitForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    if (upd == "") {
+        const vinyl = {
+            id: Date.now(),
+            albumName: album.value,
+            bandName: band.value,
+            genre: genre.value,
+            photoLink: photoLink.value,
+            type: type.value
+        };
+        vinyls.push(vinyl);
+    }
+    else {
+        let vinyl = getVynilById(upd);
+        vinyl.albumName = album.value;
+        vinyl.bandName = band.value;
+        vinyl.genre = genre.value;
+        vinyl.photoLink = photoLink.value;
+        vinyl.type = type.value;
+    }
+
+    upd = "";
+
+    saveVinylToLocalStorage(vinyls);
+    renderVinyls(vinyls);
+    submitForm.reset();
+});
